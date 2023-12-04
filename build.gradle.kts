13602970880f220cbc0357cf8e4b615d1081b6a5
Phillipp Glanz <p.glanz@madfix.me>
2023-12-04 10:27:31 +0100

    Add basic setup

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "net.onelitefeather"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
