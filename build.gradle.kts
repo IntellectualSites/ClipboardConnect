9c223d7722d6ebe9b23cc719974026ff37a3b54d
Phillipp Glanz <p.glanz@madfix.me>
2023-12-04 10:27:47 +0100

    Modify root build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
 }
 
 group = "net.onelitefeather"
-version = "1.0-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
-}
