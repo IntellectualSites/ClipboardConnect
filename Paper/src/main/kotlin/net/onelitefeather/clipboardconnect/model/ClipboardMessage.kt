c923a2621ec112ee597b541d3c5bc2b06e803694
Phillipp Glanz <p.glanz@madfix.me>
2023-12-06 15:03:34 +0100

    Fix jackson mapping for message queue

--- Paper/src/main/kotlin/net/onelitefeather/clipboardconnect/model/ClipboardMessage.kt
+++ Paper/src/main/kotlin/net/onelitefeather/clipboardconnect/model/ClipboardMessage.kt
@@ -1,8 +1,13 @@
 package net.onelitefeather.clipboardconnect.model
 
-import java.util.UUID
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import java.util.*
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
 data class ClipboardMessage(
+    @JsonProperty("userId")
     val userId: UUID,
+    @JsonProperty("fromServer")
     val fromServer: String
 )
