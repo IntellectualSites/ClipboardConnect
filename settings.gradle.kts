5f73db5853d53dce9a6cd649445ab5bf269cfdb4
Phillipp Glanz <p.glanz@madfix.me>
2023-12-06 13:55:41 +0100

    Remove dependency and replace some

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,16 +14,25 @@ include("Paper")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("worldedit", "com.sk89q.worldedit:worldedit-bukkit:7.2.17")
+            library("worldedit", "com.sk89q.worldedit","worldedit-bukkit").version("7.2.17")
 
             library("fawe.bom", "com.intellectualsites.bom", "bom-newest").version("1.39")
-            library("fawe.core", "com.intellectualsites", "FastAsyncWorldEdit-Core").withoutVersion()
-            library("fawe.bukkit", "com.intellectualsites", "FastAsyncWorldEdit-Bukkit").withoutVersion()
+            library("fawe.core", "com.fastasyncworldedit", "FastAsyncWorldEdit-Core").withoutVersion()
+            library("fawe.bukkit", "com.fastasyncworldedit", "FastAsyncWorldEdit-Bukkit").withoutVersion()
 
             library("cloud.bom", "cloud.commandframework", "cloud-bom").version("1.8.4")
             library("cloud.kotlin", "cloud.commandframework", "cloud-kotlin-extensions").withoutVersion()
             library("cloud.paper", "cloud.commandframework", "cloud-paper").withoutVersion()
+            library("cloud.annotations", "cloud.commandframework", "cloud-annotations").withoutVersion()
             library("cloud.minecraft.extras", "cloud.commandframework", "cloud-minecraft-extras").withoutVersion()
+
+            version("ktor", "2.3.6")
+            library("ktor.core", "io.ktor", "ktor-client-core").versionRef("ktor")
+            library("ktor.cio", "io.ktor", "ktor-client-cio").versionRef("ktor")
+            library("ktor.logging", "io.ktor", "ktor-client-logging").versionRef("ktor")
+            library("ktor.auth", "io.ktor", "ktor-client-auth").versionRef("ktor")
+
+            library("redis", "org.redisson","redisson").version("3.23.4")
 
             library("kotlin.test", "org.jetbrains.kotlin", "kotlin-test").withoutVersion()
             library("aerogel", "dev.derklaro.aerogel", "aerogel").version("2.1.0")
