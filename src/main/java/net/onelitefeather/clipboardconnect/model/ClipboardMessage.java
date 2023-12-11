a36826ff3acbf147d7ccaeaf89139ceae8bbd78b
Phillipp Glanz <p.glanz@madfix.me>
2023-12-11 14:01:59 +0100

    Convert ClipboardMessage class from Kotlin to Java
    ClipboardMessage class has been migrated from Kotlin to Java corresponding changes were made to the SyncService class. The code uses TypedJsonJacksonCodec instead of JsonJacksonCodec and direct getter methods were used for extracting userId and fromServer data from the message. The uncaught exceptions also now handled properly.

---
+++ src/main/java/net/onelitefeather/clipboardconnect/model/ClipboardMessage.java
@@ -0,0 +1,65 @@
+package net.onelitefeather.clipboardconnect.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import java.util.Objects;
+import java.util.UUID;
+
+/*@JsonTypeInfo(
+        use = JsonTypeInfo.Id.CLASS,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "@class"
+)*/
+public final class ClipboardMessage {
+
+    @JsonProperty("userId")
+    private UUID userId;
+    @JsonProperty("fromServer")
+    private String fromServer;
+
+    public ClipboardMessage() {
+    }
+
+    public ClipboardMessage(
+            @JsonProperty("userId")
+            UUID userId,
+            @JsonProperty("fromServer")
+            String fromServer
+    ) {
+        this.userId = userId;
+        this.fromServer = fromServer;
+    }
+
+    @JsonProperty("userId")
+    public UUID userId() {return userId;}
+
+    @JsonProperty("fromServer")
+    public String fromServer() {return fromServer;}
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (ClipboardMessage) obj;
+        return Objects.equals(this.userId, that.userId) &&
+                Objects.equals(this.fromServer, that.fromServer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, fromServer);
+    }
+
+    @Override
+    public String toString() {
+        return "ClipboardMessage[" +
+                "userId=" + userId + ", " +
+                "fromServer=" + fromServer + ']';
+    }
+
+
+}
